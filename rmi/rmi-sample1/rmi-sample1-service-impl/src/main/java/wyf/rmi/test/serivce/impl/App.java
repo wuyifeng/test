89230d2433af794aac57ee8fe982409640f21cdf
yifeng <java_feng@sian.com>
2015-12-31 08:57:10 +0000

    rmi base project

---
+++ rmi/rmi-sample1/rmi-sample1-service-impl/src/main/java/wyf/rmi/test/serivce/impl/App.java
@@ -0,0 +1,13 @@
+package wyf.rmi.test.serivce.impl;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
